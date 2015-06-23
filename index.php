325a8c22904758c2b1585af87cb7ffe9a58a0168
yene <yene@cocoadojo.com>
2015-06-23 20:23:37 +0200

    styling page and adding featuers

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+require_once "SteamSignIn.php";
+$steam_login_verify = SteamSignIn::validate();
+if (!empty($steam_login_verify)) {
+  $server = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
+  header("Location: " . $server . "?ids=" . $steam_login_verify);
+  die();
+}
+?>
 <!doctype html>
 <html class="no-js" lang="en">
   <head>
@@ -8,19 +17,26 @@
     <link rel="stylesheet" href="css/app.css" />
     <script src="js/vendor/modernizr.js"></script>
   </head>
-  <body>
-
+  <body><!--
     <div class="row">
       <div class="large-12 columns">
         <h1>Welcome to Steam Overlap</h1>
       </div>
-    </div>
+    </div>-->
 
     <div class="row">
       <div class="large-12 columns">
       	<div class="panel">
-	        <h3>We&rsquo;re stoked you want to try Steam Overlap! </h3>
-	        <p>To get going, this file (index.html) includes some basic styles you can modify, play around with, or totally destroy to get going.</p>
+	        <h3>Welcome to Steam Overlap</h3>
+	        <p>Find out which Steam games you have in common with your friends.</p>
+          <form>
+            <div class="row">
+              <div class="small-8 columns">
+                <input id="checkbox1" type="checkbox"><label for="checkbox1">Show Singleplayer</label>
+                <input id="checkbox2" type="checkbox"><label for="checkbox2">Hide games nobody played.</label>
+              </div>
+            </div>
+          </form>
       	</div>
       </div>
     </div>
@@ -29,11 +45,8 @@
       <div class="large-12 medium-12 columns">
         <div class="row">
           <div class="large-3 medium-3 small-3 columns">
-            <div class="callout panel" id="first">
-              <a href="#" class="button" id="btn-add-yourself">Add Yourself</a>
-              <p>or</p>
-              <a href="#" class="button">Add a Friend</a>
-              <p>Games</p>
+            <div class="callout panel games disable-list">
+              <a href="#" class="button"  data-reveal-id="myModal">Add Yourself</a>
               <ul class="games-list">
                 <li id="11"><a title="Counter-Strike: Source" href="http://store.steampowered.com/app/240/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/240/ee97d0dbf3e5d5d59e69dc20b98ed9dc8cad5283.jpg" alt="Counter-Strike: Source"></a></li>
                 <li id="37"><a title="Left 4 Dead" href="http://store.steampowered.com/app/500/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/500/0f67ee504d8f04ecd83986dd7855821dc21f7a78.jpg" alt="Left 4 Dead"></a></li>
@@ -50,25 +63,94 @@
             </div>
           </div>
           <div class="large-3 medium-3 small-3 columns">
-            <div class="callout panel">
+            <div class="callout panel games disable-list disable-button">
               <a href="#" class="button">Add a Friend</a>
-              <p>Games</p>
+              <ul class="games-list">
+                <li id="11"><a title="Counter-Strike: Source" href="http://store.steampowered.com/app/240/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/240/ee97d0dbf3e5d5d59e69dc20b98ed9dc8cad5283.jpg" alt="Counter-Strike: Source"></a></li>
+                <li id="37"><a title="Left 4 Dead" href="http://store.steampowered.com/app/500/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/500/0f67ee504d8f04ecd83986dd7855821dc21f7a78.jpg" alt="Left 4 Dead"></a></li>
+                <li id="47"><a title="Left 4 Dead 2" href="http://store.steampowered.com/app/550/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142.jpg" alt="Left 4 Dead 2"></a></li>
+                <li id="50"><a title="Call of Duty: Modern Warfare 2 - Multiplayer" href="http://store.steampowered.com/app/10190/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/10190/070cad064b3a81d6ede0f6054d9eb763004a07e0.jpg" alt="Call of Duty: Modern Warfare 2 - Multiplayer"></a></li>
+                <li id="53"><a title="Men of War" href="http://store.steampowered.com/app/7830/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/7830/b34d63eb274c8fe1e343e67d54e6d929dcad4afb.jpg" alt="Men of War"></a></li>
+                <li id="57"><a title="Supreme Commander 2" href="http://store.steampowered.com/app/40100/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/40100/b7a38d7f959d3e76f71d7a6d8a6bfb3e89b4e7d0.jpg" alt="Supreme Commander 2"></a></li>
+                <li id="59"><a title="Battlefield: Bad Company 2" href="http://store.steampowered.com/app/24960/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/24960/969a07d20786b5f536122d70f2c3eedc1eca5ede.jpg" alt="Battlefield: Bad Company 2"></a></li>
+                <li id="69"><a title="Sid Meier" s="" civilization="" v'="" href="http://store.steampowered.com/app/8930/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/8930/2203f62bd1bdc75c286c13534e50f22e3bd5bb58.jpg" alt="Sid Meier" s="" civilization="" v'=""></a></li>
+                <li id="70"><a title="Star Ruler" href="http://store.steampowered.com/app/70900/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/70900/47cf21bd3ed9d3180a9109ed5c91002b5e013af8.jpg" alt="Star Ruler"></a></li>
+                <li id="71"><a title="Lost Planet 2" href="http://store.steampowered.com/app/45750/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/45750/c0378cbeecba568eaf1cf7f6ddda89b857f59e07.jpg" alt="Lost Planet 2"></a></li>
+                <li id="82"><a title="Greed Corp" href="http://store.steampowered.com/app/48950/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/48950/a7521ac7891b0ac947c3676726af0109f0f2638e.jpg" alt="Greed Corp"></a></li>
+              </ul>
             </div>
           </div>
           <div class="large-3 medium-3 small-3 columns">
-            <div class="callout panel">
+            <div class="callout panel games disable-list disable-button">
               <a href="#" class="button">Add a Friend</a>
-              <p>Games</p>
+              <ul class="games-list">
+                <li id="11"><a title="Counter-Strike: Source" href="http://store.steampowered.com/app/240/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/240/ee97d0dbf3e5d5d59e69dc20b98ed9dc8cad5283.jpg" alt="Counter-Strike: Source"></a></li>
+                <li id="37"><a title="Left 4 Dead" href="http://store.steampowered.com/app/500/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/500/0f67ee504d8f04ecd83986dd7855821dc21f7a78.jpg" alt="Left 4 Dead"></a></li>
+                <li id="47"><a title="Left 4 Dead 2" href="http://store.steampowered.com/app/550/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142.jpg" alt="Left 4 Dead 2"></a></li>
+                <li id="50"><a title="Call of Duty: Modern Warfare 2 - Multiplayer" href="http://store.steampowered.com/app/10190/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/10190/070cad064b3a81d6ede0f6054d9eb763004a07e0.jpg" alt="Call of Duty: Modern Warfare 2 - Multiplayer"></a></li>
+                <li id="53"><a title="Men of War" href="http://store.steampowered.com/app/7830/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/7830/b34d63eb274c8fe1e343e67d54e6d929dcad4afb.jpg" alt="Men of War"></a></li>
+                <li id="57"><a title="Supreme Commander 2" href="http://store.steampowered.com/app/40100/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/40100/b7a38d7f959d3e76f71d7a6d8a6bfb3e89b4e7d0.jpg" alt="Supreme Commander 2"></a></li>
+                <li id="59"><a title="Battlefield: Bad Company 2" href="http://store.steampowered.com/app/24960/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/24960/969a07d20786b5f536122d70f2c3eedc1eca5ede.jpg" alt="Battlefield: Bad Company 2"></a></li>
+                <li id="69"><a title="Sid Meier" s="" civilization="" v'="" href="http://store.steampowered.com/app/8930/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/8930/2203f62bd1bdc75c286c13534e50f22e3bd5bb58.jpg" alt="Sid Meier" s="" civilization="" v'=""></a></li>
+                <li id="70"><a title="Star Ruler" href="http://store.steampowered.com/app/70900/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/70900/47cf21bd3ed9d3180a9109ed5c91002b5e013af8.jpg" alt="Star Ruler"></a></li>
+                <li id="71"><a title="Lost Planet 2" href="http://store.steampowered.com/app/45750/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/45750/c0378cbeecba568eaf1cf7f6ddda89b857f59e07.jpg" alt="Lost Planet 2"></a></li>
+                <li id="82"><a title="Greed Corp" href="http://store.steampowered.com/app/48950/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/48950/a7521ac7891b0ac947c3676726af0109f0f2638e.jpg" alt="Greed Corp"></a></li>
+              </ul>
             </div>
           </div>
           <div class="large-3 medium-3 small-3 columns">
-            <div class="callout panel">
+            <div class="callout panel games disable-list disable-button">
               <a href="#" class="button">Add a Friend</a>
-              <p>Games</p>
+              <ul class="games-list">
+                <li id="11"><a title="Counter-Strike: Source" href="http://store.steampowered.com/app/240/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/240/ee97d0dbf3e5d5d59e69dc20b98ed9dc8cad5283.jpg" alt="Counter-Strike: Source"></a></li>
+                <li id="37"><a title="Left 4 Dead" href="http://store.steampowered.com/app/500/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/500/0f67ee504d8f04ecd83986dd7855821dc21f7a78.jpg" alt="Left 4 Dead"></a></li>
+                <li id="47"><a title="Left 4 Dead 2" href="http://store.steampowered.com/app/550/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142.jpg" alt="Left 4 Dead 2"></a></li>
+                <li id="50"><a title="Call of Duty: Modern Warfare 2 - Multiplayer" href="http://store.steampowered.com/app/10190/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/10190/070cad064b3a81d6ede0f6054d9eb763004a07e0.jpg" alt="Call of Duty: Modern Warfare 2 - Multiplayer"></a></li>
+                <li id="53"><a title="Men of War" href="http://store.steampowered.com/app/7830/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/7830/b34d63eb274c8fe1e343e67d54e6d929dcad4afb.jpg" alt="Men of War"></a></li>
+                <li id="57"><a title="Supreme Commander 2" href="http://store.steampowered.com/app/40100/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/40100/b7a38d7f959d3e76f71d7a6d8a6bfb3e89b4e7d0.jpg" alt="Supreme Commander 2"></a></li>
+                <li id="59"><a title="Battlefield: Bad Company 2" href="http://store.steampowered.com/app/24960/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/24960/969a07d20786b5f536122d70f2c3eedc1eca5ede.jpg" alt="Battlefield: Bad Company 2"></a></li>
+                <li id="69"><a title="Sid Meier" s="" civilization="" v'="" href="http://store.steampowered.com/app/8930/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/8930/2203f62bd1bdc75c286c13534e50f22e3bd5bb58.jpg" alt="Sid Meier" s="" civilization="" v'=""></a></li>
+                <li id="70"><a title="Star Ruler" href="http://store.steampowered.com/app/70900/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/70900/47cf21bd3ed9d3180a9109ed5c91002b5e013af8.jpg" alt="Star Ruler"></a></li>
+                <li id="71"><a title="Lost Planet 2" href="http://store.steampowered.com/app/45750/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/45750/c0378cbeecba568eaf1cf7f6ddda89b857f59e07.jpg" alt="Lost Planet 2"></a></li>
+                <li id="82"><a title="Greed Corp" href="http://store.steampowered.com/app/48950/"><img src="http://media.steampowered.com/steamcommunity/public/images/apps/48950/a7521ac7891b0ac947c3676726af0109f0f2638e.jpg" alt="Greed Corp"></a></li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
+    </div>
+
+    <div class="row">
+      <div class="large-12 columns">
+        <footer>
+          <a href="http://steampowered.com">Powered by Steam</a>
+        </footer>
+      </div>
+    </div>
+
+
+    <div id="myModal" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+      <h2 id="modalTitle">Enter your Steam ID</h2>
+      <p class="lead">For example: 76561197964515697</p>
+      <form>
+        <div class="row">
+          <div class="large-12 columns">
+            <div class="row collapse">
+              <div class="small-10 columns">
+                <input id="input-steamid" type="text" placeholder="Steam ID">
+              </div>
+              <div class="small-2 columns">
+                <a id="btn-add-yourself" href="#" class="button postfix">Go</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+      <p class="lead">or login in through steam</p>
+      <a href="<?=SteamSignIn::genUrl()?>"><img src="img/sits_large_noborder.png"></a>
+
+
+      <a class="close-reveal-modal" aria-label="Close">&#215;</a>
     </div>
 
     <script src="js/vendor/jquery.js"></script>
--- js/app.js
+++ js/app.js
@@ -1,14 +1,62 @@
-$( "#btn-add-yourself" ).on("click", function() {
-  $.getJSON( "games.php?steamid=76561197964515697&singleplayer=0&freegames=0&played=1", function( data ) {
-    var items = [];
-    $.each( data, function( key, val ) {
-      items.push( "<li id='" + key + "'><a title='" + val.name  + "' href='http://store.steampowered.com/app/" + val.appid + "/'><img src='http://media.steampowered.com/steamcommunity/public/images/apps/" +
-        val.appid + "/" + val.img_logo_url + ".jpg' alt='" + val.name  + "' ></a></li>" );
-    });
+var ids = []; // array, index is the column
+var games = []; // array, index is the column
 
-    $( "<ul/>", {
-      "class": "games-list",
-      html: items.join( "" )
-    }).appendTo( "#first" ); // TODO remove first
+$( document ).ready(function() {
+
+  playerInfoForURL("http://steamcommunity.com/id/yene/?xml=1");
+
+  var param = getUrlParameter('ids');
+  if (param) {
+    ids = param.split(",");
+    ids.forEach(function(element, index, array) {
+      if (index > 3) {
+        // TODO add more columns
+        return;
+      }
+
+      if (index < 3) {
+        $( ".games:eq(" + (index+1) + ")").removeClass("disable-button");
+      }
+
+      $.getJSON( "games.php?steamid=" + element + "&singleplayer=0&freegames=0&played=1", function( data ) {
+        games[index] = data;
+        showResultInColumn(data, index);
+        $( ".games:eq(" + index + ")").removeClass("disable-list");
+        $( ".games:eq(" + index + ")").removeClass("disable-button");
+      });
+    });
+  }
+
+});
+
+
+$( "#btn-add-yourself" ).on("click", function() {
+  $('#myModal').foundation('reveal', 'close');
+  var steamid = $('#input-steamid').val();
+  $.getJSON( "games.php?steamid=" + steamid + "&singleplayer=0&freegames=0&played=1", function( data ) {
+    showResultInColumn(data, 0);
   });
-})
+  // change button to edit ->
+});
+
+function showResultInColumn(result, column) {
+  var items = [];
+  $.each( result, function( key, val ) {
+    items.push( "<li id='" + key + "'><a title='" + val.name  + "' href='http://store.steampowered.com/app/" + val.appid + "/'><img src='http://media.steampowered.com/steamcommunity/public/images/apps/" +
+      val.appid + "/" + val.img_logo_url + ".jpg' alt='" + val.name  + "' ></a></li>" );
+  });
+  $( ".games:eq(" + column + ") ul.games-list").empty().append(items.join( "" ));
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}
