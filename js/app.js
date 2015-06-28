92071839a34ea29989d84c8cc4fcf6a9d219eca6
yene <yene@cocoadojo.com>
2015-06-28 13:57:57 +0200

    showing result on the site

--- js/app.js
+++ js/app.js
@@ -5,33 +5,33 @@ $( document ).ready(function() {
 
   var param = getUrlParameter('ids');
   if (param) {
-    ids = param.split(",");
-    ids.forEach(function(element, index, array) {
-      if (index > 3) {
-        // TODO add more columns
-        return;
-      }
+    $.getJSON( "games.php?steamids=" + param + "&singleplayer=0&freegames=0&played=1", function( data ) {
+      console.log(data);
+      data.forEach(function(element, index, array) {
+        if (index > 3) {
+          // TODO add more columns
+          return;
+        }
+        if (index < 3) {
+          $( ".games:eq(" + (index+1) + ")").removeClass("disable-button");
+        }
+        showResultInColumn(element.games, index);
 
-      if (index < 3) {
-        $( ".games:eq(" + (index+1) + ")").removeClass("disable-button");
-      }
+        $( ".games:eq(" + index + ") .button").text(element.personaname);
+        $( ".games:eq(" + index + ") .button").css("background-image", "url(" + element.avatar + ")");
 
-      $.getJSON( "games.php?steamid=" + element + "&singleplayer=0&freegames=0&played=1", function( data ) {
-        games[index] = data;
-        showResultInColumn(data, index);
         $( ".games:eq(" + index + ")").removeClass("disable-list");
         $( ".games:eq(" + index + ")").removeClass("disable-button");
       });
     });
   }
-
 });
 
 
 $( "#btn-add-yourself" ).on("click", function() {
   $('#myModal').foundation('reveal', 'close');
   var steamid = $('#input-steamid').val();
-  $.getJSON( "games.php?steamid=" + steamid + "&singleplayer=0&freegames=0&played=1", function( data ) {
+  $.getJSON( "games.php?steamid=" + steamid + "&singleplayer=0&freegames=0", function( data ) {
     showResultInColumn(data, 0);
   });
   // change button to edit ->
@@ -43,7 +43,13 @@ $( "#btn-add-yourself" ).on("click", function() {
 function showResultInColumn(result, column) {
   var items = [];
   $.each( result, function( key, val ) {
-    items.push( "<li id='" + key + "'><a title='" + val.name  + "' href='http://store.steampowered.com/app/" + val.appid + "/'><img src='http://media.steampowered.com/steamcommunity/public/images/apps/" +
+    var style = "";
+    if (!val.owned) {
+      style = "class='disabled-game'";
+    }
+
+    items.push( "<li id='" + key + "'><a title='" + val.name  + "' href='http://store.steampowered.com/app/" + val.appid +
+      "/'><img " + style + " src='http://media.steampowered.com/steamcommunity/public/images/apps/" +
       val.appid + "/" + val.img_logo_url + ".jpg' alt='" + val.name  + "' ></a></li>" );
   });
   $( ".games:eq(" + column + ") ul.games-list").empty().append(items.join( "" ));
