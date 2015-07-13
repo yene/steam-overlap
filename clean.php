3a73452a2ce0dee646fc1497365bf6f863986b1b
yene <yene@cocoadojo.com>
2015-07-13 12:30:04 +0200

    fixed cronjob

--- clean.php
+++ clean.php
@@ -1,3 +1,4 @@
 <?php
-$cmd = 'find cache/ -name "*.json" -type f -mtime +10 -delete';
+$currentDir = realpath(dirname(__FILE__));
+$cmd = "find $currentDir/cache/ -name \"*.json\" -type f -mtime +10 -delete";
 exec($cmd);
